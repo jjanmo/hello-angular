363faf7201cd8857f3ced07d141a3b8e2f8b335e
jjanmo <jjanmo07@hanmail.net>
2021-09-22 00:58:31 +0900

    feat: add logic of counting feature

--- src/app/section/display/display.component.ts
+++ src/app/section/display/display.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
-  styleUrls: ['./display.component.scss']
+  styleUrls: ['./display.component.scss'],
 })
 export class DisplayComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  count = 0;
+  constructor() {
+    this.setCountInterval();
   }
 
+  setCountInterval() {
+    setInterval(() => {
+      console.log(this);
+      this.count++;
+    }, 1000);
+  }
+
+  ngOnInit(): void {}
 }
